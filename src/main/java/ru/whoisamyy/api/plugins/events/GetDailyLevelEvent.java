6ba134265c532442ef2dfbc2b0db014de77ea593
whoisamyy <egegegg002@gmail.com>
2023-11-29 16:41:55 +0300

    new event

---
+++ src/main/java/ru/whoisamyy/api/plugins/events/GetDailyLevelEvent.java
@@ -0,0 +1,31 @@
+package ru.whoisamyy.api.plugins.events;
+
+import ru.whoisamyy.core.endpoints.RequestManager;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class GetDailyLevelEvent extends Event {
+    @Override
+    void init() {
+        try {
+            Method m = RequestManager.Levels.class.getMethod("getGJDailyLevel", Boolean.class);
+            List<Class<?>> at = new ArrayList<>(List.of(m.getParameterTypes()));
+            at.add(m.getReturnType());
+            allowedTypes = at;
+            Parameter[] params= m.getParameters();
+            List<String> strings = new ArrayList<>();
+            Arrays.stream(params).forEach(x->strings.add(x.getName()));
+            parameterNames = new ArrayList<>(strings);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public GetDailyLevelEvent(Object... parameterValues) throws NoSuchFieldException {
+        super(parameterValues);
+    }
+}
